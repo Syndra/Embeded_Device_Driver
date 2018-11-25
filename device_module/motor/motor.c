8571d8aa19ec26cd25129d8108e18008b4d1c88a
lenny-kong <forlennykong@gmail.com>
2018-11-25 17:34:40 +0900

    Update motor.c

--- device_module/motor/motor.c
+++ device_module/motor/motor.c
@@ -54,15 +54,12 @@ static void __motor_write_from_int(int num)
 	if(num == 768)
 	{
 		iowrite8(0x00, motor_ioremap);
-		motor_ioremap++; motor_ioremap++;
-		iowrite8(0x00, motor_ioremap);
-		motor_ioremap--; motor_ioremap--;
 	}
 	else
 	{
 		iowrite8(0x01, motor_ioremap);
 		motor_ioremap++; motor_ioremap++;
-		iowrite8(0xFF, motor_ioremap);
+		iowrite8(0x05, motor_ioremap);
 		motor_ioremap--; motor_ioremap--;
 	}
 }
